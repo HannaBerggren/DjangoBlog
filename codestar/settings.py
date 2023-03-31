d3a6631e7ee7910b44ea201d9eabf67a3f8a0514
HannaBerggren <hannajberggren@gmail.com>
2023-03-31 13:47:42 +0000

    changed my allowed host

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,11 +30,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["codestar-hanna.herokuapp.com", "localhost"]
 
 # Application definition
 
